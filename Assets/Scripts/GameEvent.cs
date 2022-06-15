16fe4442dae6b4a0fed37c3a9aefe70f4ce33be0
GiovanniCrudo00 <crugio23@gmail.com>
2022-06-15 18:44:02 +0200

    Aggiunto menu in game, resume game funzionante

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent
+{
+    public static bool isPaused = false;
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private SettingPopup settingsPopup;
+    private int _score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingsPopup.Close();
+    }
+    private void OnEnemyHit()
+    {
+    }
+    // Update is called once per frame
+    void Update() //TEST CODE
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            settingsPopup.Open();
+        }
+    }
+    public void OnOpenSettings()
+    {
+        Debug.Log("Open Settings");
+        settingsPopup.Open();
+    }
+    public void OnPointerDown()
+    {
+        Debug.Log("Pointer Down!");
+    }
+}
