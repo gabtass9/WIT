09367adcf48e2082d40aaaa22e7281535b5df420
GiovanniCrudo00 <crugio23@gmail.com>
2022-06-17 19:51:42 +0200

    BUG SULLE ANIMAZIONI DELLA PERRI, FREESBEE THROW NON SI ATTIVA

--- Assets/Scripts/pathfinding.cs
+++ Assets/Scripts/pathfinding.cs
@@ -17,7 +17,8 @@ public class pathfinding : MonoBehaviour
     void Start()
     {
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        _animator.SetFloat("Speed", 0.2f);
+        _animator.SetFloat("Speed", 1.0f);
+        _animator.SetBool("IsAttacking",false);
     }
 
     // Update is called once per frame
@@ -28,13 +29,16 @@ public class pathfinding : MonoBehaviour
         if(distance > 10)
         {
             nav.SetDestination(target.position);
-            _animator.SetFloat("Speed", 0.2f);
+            _animator.SetFloat("Speed", 1.0f);
             _animator.SetBool("IsAttacking",false);
         }
         else
         {
             nav.SetDestination(transform.position);
             _animator.SetFloat("Speed", 0.0f);
+            Vector3 lookVector = target.position - transform.position;                   //***********************************************************************
+            Quaternion rot = Quaternion.LookRotation(lookVector);                    //Con questo codice il boss se si ferma mi guarda sempre, tiene lo sguardo fisso su di me
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);      //************************************************************************
             RaycastHit hit;
             Ray ray = new Ray(transform.position, transform.forward);
             if(Physics.SphereCast(ray,0.75f,out hit))
@@ -43,12 +47,15 @@ public class pathfinding : MonoBehaviour
                 if(hitObject.GetComponent<PlayerCharacter>())
                 {
                     _animator.SetBool("IsAttacking",true);
+                    /*
+                        L' ANIMAZIONE NON VIENE FATTA
+                    */
                      if(_fireball == null)
                     {
                         _fireball = Instantiate(fireballPrefab) as GameObject;
-                        //_fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
                         _fireball.transform.position = transform.position+new Vector3(0,2.0f,1.5f);
                         _fireball.transform.rotation = transform.rotation;
+                        
                     }
                 }
             }
